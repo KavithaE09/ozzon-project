542593132995bef6b5a18b06aa66cc8f52a11d6f
Sasikala124 <sasikala2004v@gmail.com>
2026-02-05 01:05:29 +0530

    materiallistfield

--- src/components/MasterForms/MaterialGroup.jsx
+++ src/components/MasterForms/MaterialGroup.jsx
@@ -2,13 +2,14 @@ import MasterForm from './singlefieldMasterForm';
 import { getAllMaterialGroups, createMaterialGroup, updateMaterialGroup, deleteMaterialGroup } from '../../api/masterApi';
 
 export default function MaterialGroupMaster() {
-  return <MasterForm
-    title="Material Group"
-    fetchAll={getAllMaterialGroups}
-    createItem={createMaterialGroup}
-    updateItem={updateMaterialGroup}
-    deleteItem={deleteMaterialGroup}
-    nameKey="MaterialGroup"
-    idKey="MaterialGroupMId"
-  />
+  return<MasterForm
+  title="Material Group"
+  fetchAll={getAllMaterialGroups}
+  createItem={createMaterialGroup}
+  updateItem={updateMaterialGroup}
+  deleteItem={deleteMaterialGroup}
+  nameKey="MaterialGroupName"  
+  idKey="MaterialGroupId"      
+/>
+
 }
