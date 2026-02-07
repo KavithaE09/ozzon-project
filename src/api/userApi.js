137b066fb7cbe5f103fcfe860ee56b0a920dc5cf
Sasikala124 <sasikala2004v@gmail.com>
2026-02-07 11:54:46 +0530

    base

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
+import { BASE_URL } from "./baseurl";
 
-const BASE_URL = 'http://localhost:5000/api';
+const API_URL = `${BASE_URL}/users`;
 
 const userApi = {
-  // Get all users
   getAllUsers: async () => {
     try {
-      const response = await axios.get(`${BASE_URL}/users`);
+      const response = await axios.get(API_URL);
       return response.data;
     } catch (error) {
       console.error('Get users error:', error);
@@ -14,10 +14,9 @@ const userApi = {
     }
   },
 
-  // Create user
   createUser: async (userData) => {
     try {
-      const response = await axios.post(`${BASE_URL}/users`, {
+      const response = await axios.post(API_URL, {
         username: userData.username,
         departmentId: userData.departmentId,
         password: userData.password,
@@ -31,10 +30,9 @@ const userApi = {
     }
   },
 
-  // Update user
   updateUser: async (userId, userData) => {
     try {
-      const response = await axios.put(`${BASE_URL}/users/${userId}`, {
+      const response = await axios.put(`${API_URL}/${userId}`, {
         username: userData.username,
         departmentId: userData.departmentId,
         password: userData.password,
@@ -48,10 +46,9 @@ const userApi = {
     }
   },
 
-  // Delete user
   deleteUser: async (userId) => {
     try {
-      const response = await axios.delete(`${BASE_URL}/users/${userId}`);
+      const response = await axios.delete(`${API_URL}/${userId}`);
       return response.data;
     } catch (error) {
       console.error('Delete user error:', error);
@@ -59,10 +56,9 @@ const userApi = {
     }
   },
 
-  // Search users
   searchUsers: async (searchTerm) => {
     try {
-      const response = await axios.get(`${BASE_URL}/users/search?username=${searchTerm}`);
+      const response = await axios.get(`${API_URL}/search?username=${encodeURIComponent(searchTerm)}`);
       return response.data;
     } catch (error) {
       console.error('Search users error:', error);
